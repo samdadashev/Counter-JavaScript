438f1bba5549d6d2df07a70af6a2f14f8b84bec3
samdadashev <77366348+samdadashev@users.noreply.github.com>
2022-10-08 15:18:45 +0200

    Update app.js

--- Counter-JavaScript/app.js
+++ Counter-JavaScript/app.js
@@ -1,17 +1,20 @@
 var count = 0;
 
 function add() {
-    count += 1;
-    document.getElementById('counter').innerHTML = count;
+	count += 1;
+	document.getElementById("lower").disabled = false;
+	document.getElementById('counter').innerHTML = count;
 }
 function lower() {
-    if (count == 0) {
-        window.alert('Die nummer darf nicht kleiner als Null sein '
-        )
-        count = 0;
-    } else {
-        count--;
-        document.getElementById('counter').innerHTML = count;
+	if (count == 0) {
+		count = 0;
+		document.getElementById("lower").disabled = true;
 
-    }
-}
+		window.alert('Die nummer darf nicht kleiner als Null sein '
+		);
+	} else {
+		count--;
+		document.getElementById('counter').innerHTML = count;
+
+	}
+}a
